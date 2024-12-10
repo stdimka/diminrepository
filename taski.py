bb43ef6f274abc4567ecc92f795750f4af4aaa1f
stdimka <dimkadimko88@gmail.com>
2024-12-11 00:26:11 +0200

    сдесь задачи с javarush

---
+++ taski.py
@@ -0,0 +1,21 @@
+list1 = ["Alice", "Bob", "Charlie"]
+list2 = [25, 30, 35]
+
+# Объединение двух списков в список кортежей с использованием zip()
+zipped_list = list(zip(list1, list2))
+print("Объединенный список кортежей:", zipped_list)
+
+# Список чисел
+numbers = [10, 20, 5, 15, 30]
+
+# Наименьший элемент в списке чисел
+min_number = min(numbers)
+print("Наименьший элемент в списке чисел:", min_number)
+
+# Наибольший элемент в списке чисел
+max_number = max(numbers)
+print("Наибольший элемент в списке чисел:", max_number)
+
+# Сумма всех элементов в списке чисел
+sum_numbers = sum(numbers)
+print("Сумма всех элементов в списке чисел:", sum_numbers)
